82b8ae8b4f033ccaa13a8492e2898d81c27ee60c
Marco Martins <marko.double@gmail.com>
2023-03-12 13:59:51 +0100

    Added visual month helper to the tests

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -2,6 +2,19 @@ import { describe, it, expect } from "vitest";
 import { SimpleCalendar } from "./lib";
 import { weekDays, weekDaysAbbr } from "./utils";
 
+/** 
+Calendar test month (February 2023)
+
+Sun, Mon Tue, Wed, Thu, Fri, Sat
+
+[29, 30, 31, 1, 2, 3, 4,
+ 5, 6, 7, 8, 9, 10, 11,
+ 12, 13, 14, 15, 16, 17, 18,
+ 19, 20, 21, 22, 23, 24, 25,
+ 26, 27, 28, 1, 2, 3, 4]
+ 
+*/
+
 describe("Simple calendar lib", () => {
   it("should return a month calendar for the given date", () => {
     const date = new Date("2023-02-11");
